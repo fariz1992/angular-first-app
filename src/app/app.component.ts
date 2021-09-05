1a225d44ce1ed5ff159c7b833e53ae9f6e309ae6
Fariz Huseynov <fariz.huseynov20@gmail.com>
2021-09-05 19:16:54 +0300

    toaster kurulum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy { 
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Senan';
+  constructor(private toastr: ToastrService) { }
+
   ngOnInit(): void {
     console.log("Sehife ilk acilanda cagirilacaq funksiya.")
+    this.showSuccess();
   }
 
   ngOnDestroy(): void {
     console.log("Sehife baglananda cagirilacaq funksiya.")
   }
-  title = 'Senan';
-  
+
+  showSuccess = () => {
+    const myConfig = {
+      closeButton: true,
+      progressBar: true,
+      timeOut:10000
+    };
+    this.toastr.success('Satış başarıyla tamamlandı.', 'Tebrikler'/*, myConfig*/);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      progressBar: true,
+      timeOut:10000
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
