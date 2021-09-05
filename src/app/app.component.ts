2b0f73f5c3d7e84e718254033efc0aa9d31dbd1d
Fariz Huseynov <fariz.huseynov20@gmail.com>
2021-09-05 18:16:00 +0300

    first app files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'first-app';
+export class AppComponent implements OnInit, OnDestroy { 
+  ngOnInit(): void {
+    console.log("Sehife ilk acilanda cagirilacaq funksiya.")
+  }
+
+  ngOnDestroy(): void {
+    console.log("Sehife baglananda cagirilacaq funksiya.")
+  }
+  title = 'Senan';
+  
 }
