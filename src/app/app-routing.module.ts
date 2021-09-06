9a6596c475276b09d8182efd58231e4315c045b1
Fariz Huseynov <fariz.huseynov20@gmail.com>
2021-09-06 19:20:22 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent
   },
   {
--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
+  private userId: string;
+  constructor(private router: ActivatedRoute,private toastr: ToastrService) {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    this.router.params.subscribe(params => {
+      this.userId = params['id'].toString();
+      this.toastr.success(this.userId);
+    });
   }
 
 }
