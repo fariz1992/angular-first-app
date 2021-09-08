25d7a5526f62d2551583f9889d4d0716cdb55e59
Fariz Huseynov <fariz.huseynov20@gmail.com>
2021-09-08 14:16:24 +0300

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { UserComponent } from './user/user.component';
@@ -16,6 +16,15 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserSettingsComponent } from './user/user-settings/user-settings.component';
 import { UserPermissionsComponent } from './user/user-permissions/user-permissions.component';
 import { UserPostsComponent } from './user/user-posts/user-posts.component';
+import {
+  IgxNavbarModule,
+  IgxIconModule,
+  IgxButtonModule,
+  IgxNavigationDrawerModule,
+  IgxRippleModule,
+  IgxToggleModule
+} from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -38,9 +47,16 @@ import { UserPostsComponent } from './user/user-posts/user-posts.component';
     ToastrModule.forRoot({
       closeButton: true,
       progressBar: true,
-      timeOut:10000
+      timeOut: 10000
     }),
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FormsModule,
+    IgxIconModule,
+    IgxNavbarModule,
+    IgxButtonModule,
+    IgxNavigationDrawerModule,
+    IgxRippleModule,
+    IgxToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent],
